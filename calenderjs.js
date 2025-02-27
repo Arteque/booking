aee16a6d7186ede0df110369d2848a35a11ec24f
Ahmed Lemssiah <art3que@gmail.com>
2025-02-27 16:32:25 +0100

    Booking Calendar

--- calenderjs.js
+++ calenderjs.js
@@ -4,7 +4,35 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     const calendar = new FullCalendar.Calendar(calendarAn, {
         initialView : 'dayGridMonth',
-        locale:"de",
+        selectable: true,
+        locale: "de-DE",
+        eventDisplay:"background",
+        eventColor:"var(--warn-100)",
+        headerToolbar:{
+            left:"title",
+            center:"",
+            right:"next prev"
+        },
+        events: allEvents,
+        eventClick:function(info){
+            console.log(info.event.title)
+        }
     })
     calendar.render()
 })
+
+
+const allEvents = [
+    {
+        id:new Date().getTime,
+        title:"",
+        start:"2025-02-10",
+        end:"2025-02-21",
+    },
+    {
+        id:new Date().getTime,
+        title:"",
+        start:"2025-02-24",
+        end:"2025-02-28",
+    }
+]
